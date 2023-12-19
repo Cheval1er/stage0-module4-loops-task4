f63feaa7a2168a5c25f4c62fe4dc1c4f9227b8cc
nutsa.matchavariani <nutsa.matchavariani@energo-pro.ge>
2023-12-19 17:57:18 +0400

    Module #4.2. Task #4. Loop Keywords

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
+        if (toBreakWith > numberToGoUntil) {
+            System.out.println("iterating till the end");
+            for (int i = 0; i < numberToGoUntil; i++) {
+                System.out.println(i + 1);
+            }
 
+        } else {
+            for (int i = 0; i < toBreakWith; i++) {
+                System.out.println(i + 1);
+if (i==toBreakWith){
+break;
+}
+            }
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -2,6 +2,43 @@ package school.mjc.stage0.loops.task4;
 
 public class TwoRangesSum {
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
+        if (numberToSkip > lastInRow) {
+            System.out.println("number to skip is bigger then the last");
+            return;
+        } else if (lastInRow < 0) {
+            System.out.println("last number in row is negative");
+            return;
+        } else {
+            int skippedSum = 0;
+            int countedSum = 0;
+            if (numberToSkip == lastInRow) {
+                for (int i = 1; i <= lastInRow; i++) {
 
-    }
-}
+                        skippedSum += i;
+                        countedSum += i;
+
+
+                    }
+                System.out.println("skipped sum is " +( skippedSum));
+                System.out.println("counted sum is " + (countedSum - skippedSum ));
+                return;
+                }}
+        int skippedSum = 0;
+        int countedSum = 0;
+                    for (int i = 1; i <= lastInRow; i++) {
+
+                        if (i == numberToSkip || i == lastInRow) {
+                            skippedSum += i;
+                            countedSum += i;
+                        } else {
+                            countedSum += i;
+                        }
+
+                    }
+            System.out.println("skipped sum is " +( skippedSum));
+            System.out.println("counted sum is " + (countedSum - skippedSum - skippedSum + numberToSkip + lastInRow));
+
+                }
+            }
+
+
